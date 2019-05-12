17c9884cfa8637c114f60ab4f9772c57c3a2a9d5
saikrishna321 <saikrishna321@yahoo.com>
2019-05-12 10:17:14 +0100

    try build fail

--- __tests__/launch.spec.js
+++ __tests__/launch.spec.js
@@ -21,5 +21,5 @@ test('Should open browser and send events', async () => {
   let version = await evaluate(
     () => navigator.appVersion.match(/.*Chrome\/([0-9.]+)/)[1]
   );
-  expect(version.result).toBe('69.0.3497.100');
+  expect(version.result).toBe('74.0.3729.136');
 });
